b84537ec815181cb8dc6c837b92c8ef89525d6d3
Mohammedal-nabahin <moha.nabahin@gmail.com>
2023-10-03 23:45:02 +0300

    Remove comments

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-const validator = require('validator')
-const chalk = require('chalk');
 const yargs = require('yargs');
-
-// const Notes = require('./notes.js');
-// const msg = Notes();
-// console.log(msg)
-
-// console.log(validator.isEmail('user@user.com'))
-// fs.writeFileSync('notes.txt' , 'Hello World');
-// fs.appendFileSync('notes.txt' , 'Hello Again!');
-
-
-// console.log(chalk.cyan.inverse.bold('Success!'))
-
-
-//Getting an input from the user , !using process golbal variable (argv) :
-
 
 // add command
 yargs.command({
